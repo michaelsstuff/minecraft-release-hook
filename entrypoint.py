6c813223c6c528b9ae91d2f5e65e76b018f8fdb4
Michael Schietzsch <schietzsch@googlemail.com>
2019-09-17 14:28:06 +0200

    rework to use http.client and urllib.parse

--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,13 @@
 #!/usr/local/bin/python
 import json
-import urllib.request
+import http.client
+import urllib.parse
 import sys
-import requests
 import os
 
 # API_ENDPOINT = "https://cloud.docker.com/api/build/v1/source/.../trigger/.../call/"
 API_ENDPOINT = os.environ['API_ENDPOINT']
+api_parsed = urllib.parse.urlparse(API_ENDPOINT)
 
 # get the last known version
 f = open(r"version.json", "r")
@@ -15,18 +16,32 @@ lastknown_snap = olddata['saved_version']
 f.close()
 
 # get the current release snapshot version
-newdata = urllib.request.urlopen(
-    "https://launchermeta.mojang.com/mc/game/version_manifest.json").read()
-output = json.loads(newdata)
+conn = http.client.HTTPSConnection("launchermeta.mojang.com")
+conn.request("GET", "/mc/game/version_manifest.json")
+read_manifest = conn.getresponse()
+if read_manifest.status != 200:
+    print("Could not fetch the version_manifest.json")
+    print(read_manifest.status, read_manifest.reason)
+    sys.exit(1)
+
+data1 = read_manifest.read()
+output = json.loads(data1)  
 latest_snap = output['latest']['snapshot']
 
 if lastknown_snap == latest_snap:
     print("Versions are equal")
-    sys.exit()
+    sys.exit(0)
 else:
     print("Newer version detected")
     print("The new version is", latest_snap)
     with open('version.json', 'w', encoding='utf-8') as f:
         json.dump({"saved_version": latest_snap}, f)
-    requests.post(url = API_ENDPOINT) 
-    sys.exit()
+    connapi = http.client.HTTPSConnection(api_parsed.hostname)
+    connapi.request("POST", api_parsed.path)
+    response = connapi.getresponse()
+    if response.status != 202:
+        print("Could not POST to API")
+        print(response.status, response.reason)
+        sys.exit(1)
+    print ("Build request was send.")
+    sys.exit(0)
